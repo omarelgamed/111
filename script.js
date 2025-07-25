4d54f83c9b995eeb54fce08f3cda732441b4fafc
Omar Kareem <ok01006806832@gmail.com>
2025-07-25 20:21:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let img=document.images[0]
+
+img.src="Escucha música y visualiza videos sin limite.jpg"
+
+console.log(img)
+
+
+
+let link=document.links[0]
+
+link.href="https://youtube.com/"
+
+link.innerHTML="google => youtube"
+
+console.log(link)
